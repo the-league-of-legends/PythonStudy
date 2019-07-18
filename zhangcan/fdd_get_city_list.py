af228cfd924d8c851ca2af4de065c794dd0e684a
canzhang <canzhang@lexinfintech.com>
2019-07-18 18:22:13 +0800

    解析全部数据

--- zhangcan/fdd_get_city_list.py
+++ zhangcan/fdd_get_city_list.py
@@ -5,7 +5,7 @@ import json
 
 # 带参数的GET请求
 r = requests.get(url='https://suzhou.fangdd.com/component/api/data/fetchCityList', params={'baseUrl': '/loupan/'})
-r.encoding = 'utf-8'  # 得到的结果转换为 gbk 编码
+r.encoding = 'utf-8'  # 得到的结果转换为编码
 print(r.status_code)  # 获取返回状态
 print(r.url)
 print(r.text)  # 打印解码后的返回数据
@@ -14,8 +14,13 @@ jsonStr = r.text
 jsonObject = json.loads(jsonStr)  # 将已经编码的Json串解码为python对象
 hotCityListObject = jsonObject["data"]["hotCityList"]  # 方式1
 print(hotCityListObject)
-hotCityList02 = jsonObject.get("data").get("hotCityList")  # 方式2
-print(hotCityList02)
-
+# hotCityList02 = jsonObject.get("data").get("hotCityList")  # 方式2
+# print(hotCityList02)
 for item in hotCityListObject:
-    print(item.get("text") + " " + item.get("url"))
+    print("text=%s,url=%s" % (item.get("text"), item.get("url")))
+
+letterCityMap = jsonObject.get("data").get("letterCityMap")
+for item in letterCityMap.items():
+    print("key=%s,value=%s" % (item[0], item[1]))
+    for temp in item[1]:
+        print("text=%s,url=%s" % (temp.get("text"), temp.get("url")))
